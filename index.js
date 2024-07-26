3556adf46ea777476598a77a7b05373ef632e21a
Martin Garcia <newluxfero@gmail.com>
2024-07-26 11:46:45 +0200

    chore: await for format results

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-
-const { existsSync, writeFileSync } = require('fs');
-const { dirname, resolve } = require('path');
-const { sync: mkdirp } = require('mkdirp');
-const { ESLint } = require('eslint');
+const { existsSync, writeFileSync } = require("fs");
+const { dirname, resolve } = require("path");
+const { sync: mkdirp } = require("mkdirp");
+const { ESLint } = require("eslint");
 
 const eslint = new ESLint();
 
-function findConfig () {
+function findConfig() {
   let root = resolve(process.cwd());
-  while (existsSync(root + '/package.json')) {
-    const config = require(root + '/package.json');
-    if (config['eslint-formatter-multiple']) {
-      return config['eslint-formatter-multiple'];
+  while (existsSync(root + "/package.json")) {
+    const config = require(root + "/package.json");
+    if (config["eslint-formatter-multiple"]) {
+      return config["eslint-formatter-multiple"];
     }
-    root = resolve(root + '/../');
+    root = resolve(root + "/../");
   }
   return null;
 }
@@ -23,22 +22,24 @@ module.exports = async function (results, args) {
   const config = findConfig();
 
   if (!config) {
-    console.error('Unable to find a package config that has eslint-formatter-multiple. See README');
+    console.error(
+      "Unable to find a package config that has eslint-formatter-multiple. See README"
+    );
     return false;
   }
 
   for (const formatterConfig of config.formatters || []) {
     const formatter = await eslint.loadFormatter(formatterConfig.name);
-    const formatterResult = formatter.format(results);
-    if (formatterConfig.output === 'console') {
+    const formatterResult = await formatter.format(results);
+    if (formatterConfig.output === "console") {
       console.log(formatterResult);
-    } else if (formatterConfig.output === 'file') {
+    } else if (formatterConfig.output === "file") {
       const filePath = resolve(root, formatterConfig.path);
       try {
         mkdirp(dirname(filePath));
         writeFileSync(filePath, formatterResult);
       } catch (ex) {
-        console.error('There was a problem writing the output file:\n%s', ex);
+        console.error("There was a problem writing the output file:\n%s", ex);
 
         return false;
       }
